7a1f849cc1ac7dfcdaaaf3d0ca721c9872bfe561
raythurman2386 <raymondthurman5@gmail.com>
2019-11-09 02:33:14 -0500

    added strict mode and fixed index

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
