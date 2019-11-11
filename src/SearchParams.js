3e5ccd6acc2607c3dc483b7de98917196fce9119
raythurman2386 <raymondthurman5@gmail.com>
2019-11-10 23:48:54 -0500

    worked up to error boundaries

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./hooks/useDropdown";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Corydon, IN");
+  const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
@@ -26,7 +27,7 @@ const SearchParams = () => {
     pet.breeds(animal).then(({ breeds }) => {
       const breedStrings = breeds.map(({ name }) => name);
       setBreeds(breedStrings);
-    }, console.error);
+    });
   }, [animal, setBreed]);
 
   return (
@@ -52,6 +53,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
